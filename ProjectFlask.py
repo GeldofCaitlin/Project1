1ca055251f567ee865bebefd5b1b90f812fc3a8a
caitlingeldof <caitlin.geldof@student.howest.be>
2017-06-06 15:12:02 +0200

    Initial commit

---
+++ ProjectFlask.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', debug=True)
